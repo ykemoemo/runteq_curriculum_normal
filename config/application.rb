c52f62e3b5ca1e786548c0f267cccae3ae0006d5
Yamashita <yk.emoemo@gmail.com>
2021-04-08 16:40:09 +0900

    Change generator setting

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,13 @@ Bundler.require(*Rails.groups)
 
 module RunteqNormal
   class Application < Rails::Application
+
+    config.generators do |g|
+      g.assets  false
+      g.test    false
+      g.helper   true
+    end
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
