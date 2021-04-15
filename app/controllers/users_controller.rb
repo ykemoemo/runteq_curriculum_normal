ed693ca1b8795378c3bd09f05635d1886fb766e6
Yamashita <yk.emoemo@gmail.com>
2021-04-15 16:47:56 +0900

    [Add} flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = 'ユーザー登録が完了しました'
       redirect_to login_path
     else
+      flash.now[:danger] = 'ユーザー登録に失敗しました'
       render :new
     end
   end
--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,6 @@ module RunteqNormal
     # Don't generate system test files.
     config.generators.system_tests = nil
 
-    config.i18n.available_locales = %i[ja en]
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
