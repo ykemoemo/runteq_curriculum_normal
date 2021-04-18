d20dc2edaad46ef49b6e8e4e23bb6ce1da1f08f9
Yamashita <yk.emoemo@gmail.com>
2021-04-18 15:50:07 +0900

    [Add] decorator

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-
   add_flash_types :success, :info, :warning, :danger
 
   def not_authenticated
--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -13,5 +13,4 @@ class UserDecorator < Draper::Decorator
   #       object.created_at.strftime("%a %m/%d/%y")
   #     end
   #   end
-
 end
