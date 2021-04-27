4e007e0527df305dd7341c269a37d83550d257c4
Yamashita <yk.emoemo@gmail.com>
2021-04-27 11:33:21 +0900

    [Add] board image upload function

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -9,7 +9,7 @@ class BoardsController < ApplicationController
 
   def create
     @board = current_user.boards.new(board_params)
-    if @board.save!
+    if @board.save
       redirect_to boards_path, success: t('.success')
     else
       flash.now[:danger] = t('.fail')
