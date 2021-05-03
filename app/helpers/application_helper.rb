b36c91a7624cf1a36b479709bc1b59ab15178291
Yamashita <yk.emoemo@gmail.com>
2021-05-03 20:17:31 +0900

    [Add] dynamic_title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,7 @@
 module ApplicationHelper
-     # 引数が渡されなかった場合は空文字をデフォルト値とする
   def page_title(page_title = '')
     base_title = 'RUNTEQ BOARD APP'
-
-    # 三項演算子
+ 
     page_title.empty? ? base_title : page_title + " | " + base_title
   end
 end
