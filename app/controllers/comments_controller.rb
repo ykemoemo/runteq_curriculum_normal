f527aab6a0e90b87e78645d9bb89bacafe2889e0
Yamashita <yk.emoemo@gmail.com>
2021-05-13 20:20:12 +0900

    [Add] comment ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,12 @@
 class CommentsController < ApplicationController
   def create
-    comment = current_user.comments.build(comment_params)
-    if comment.save
-      redirect_to board_path(comment.board), success: t('.success')
-    else
-      redirect_to board_path(comment.board), danger: t('.fail')
-    end
+    @comment = current_user.comments.build(comment_params)
+    @comment.save
+  end
+
+  def destroy
+    @comment = current_user.comments.find(params[:id])
+    @comment.destroy!
   end
 
   private
