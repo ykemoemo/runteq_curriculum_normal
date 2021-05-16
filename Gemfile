88908388e16064cd5c4c485a68e31a9ab0cbf3c3
Yamashita <yk.emoemo@gmail.com>
2021-05-16 13:08:45 +0900

    search boards

--- Gemfile
+++ Gemfile
@@ -77,3 +77,5 @@ gem 'pry-rails'
 gem 'carrierwave'
 
 gem 'kaminari'
+
+gem 'ransack'
--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,7 +2,8 @@ class BoardsController < ApplicationController
   before_action :ensure_user, only: %i[edit update destroy]
 
   def index
-    @boards = Board.all.includes(:user).order(created_at: :desc).page(params[:page])
+    @search = Board.ransack(params[:q])
+    @boards = @search.result.order(created_at: :desc).page(params[:page])
   end
 
   def new
@@ -46,7 +47,8 @@ class BoardsController < ApplicationController
   end
 
   def bookmarks
-    @bookmark_boards = current_user.bookmark_boards.includes(:user).order(created_at: :desc).page(params[:page])
+    @search = current_user.bookmark_boards.ransack(params[:q])
+    @bookmark_boards = @search.result.includes(:user).order(created_ad: :desc).page(params[:page])
   end
 
   private
