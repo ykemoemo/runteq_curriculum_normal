c0c40069c0efd676fa432cb80a8ed20f567eceff
Yamashita <yk.emoemo@gmail.com>
2021-05-21 19:47:47 +0900

    [Add] password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,35 +1,30 @@
 class PasswordResetsController < ApplicationController
   skip_before_action :require_login
 
-  def create 
-    @user = User.find_by_email(params[:email])
-    @user.deliver_reset_password_instructions! if @user
-    redirect_to root_path, success: t('.success')
+  def create
+    @user = User.find_by(email: params[:email])
+    @user&.deliver_reset_password_instructions!
+    redirect_to login_path, success: t('.success')
   end
 
   def edit
     @token = params[:id]
     @user = User.load_from_reset_password_token(params[:id])
-    if @user.blank?
-      not_authenticated
-      return
-    end
+    not_authenticated if @user.blank?
   end
 
   def update
     @token = params[:id]
     @user = User.load_from_reset_password_token(params[:id])
-    if @user.blank?
-      not_authenticated
-      return
-    end
+
+    return not_authenticated if @user.blank?
+
     @user.password_confirmation = params[:user][:password_confirmation]
     if @user.change_password(params[:user][:password])
       redirect_to login_path, success: t('.success')
     else
       flash.now[:danger] = t('.fail')
-      render :action => "edit"
+      render :edit
     end
   end
 end
-
