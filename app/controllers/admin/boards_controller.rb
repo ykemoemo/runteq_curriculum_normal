e9c4d383febed04d9163865e3f089294be46dec2
Yamashita <yk.emoemo@gmail.com>
2021-05-30 12:09:50 +0900

    [Add] admin user board crud

--- app/controllers/admin/boards_controller.rb
+++ app/controllers/admin/boards_controller.rb
@@ -1,39 +1,39 @@
 class Admin::BoardsController < Admin::BaseController
-    before_action :set_board, only: %i[edit show update destroy]
-    def index
-      @q = Board.ransack(params[:q])
-      @boards = @q.result(distinct: true).includes(:user).order(created_at: :desc).page(params[:page])
-    end
-  
-    def edit; end
-  
-    def show
-      @board = Board.find(params[:id])
-      @comment = Comment.new
-      @comments = @board.comments.includes(:user).order(created_at: :desc)
-    end
-  
-    def update
-      if @board.update(board_update_params)
-        redirect_to admin_board_path(@board), success: '掲示板を更新しました'
-      else
-        flash.now['danger'] = '掲示板を更新できませんでした'
-        render :edit
-      end
-    end
-  
-    def destroy
-      @board.destroy
-      redirect_to admin_boards_path, success: '掲示板を削除しました'
-    end
-  
-    private
-  
-    def board_update_params
-      params.require(:board).permit(:title, :body, :user_id, :board_image, :board_image_cache)
-    end
-  
-    def set_board
-      @board = Board.find(params[:id])
+  before_action :set_board, only: %i[edit show update destroy]
+  def index
+    @q = Board.ransack(params[:q])
+    @boards = @q.result(distinct: true).includes(:user).order(created_at: :desc).page(params[:page])
+  end
+
+  def edit; end
+
+  def show
+    @board = Board.find(params[:id])
+    @comment = Comment.new
+    @comments = @board.comments.includes(:user).order(created_at: :desc)
+  end
+
+  def update
+    if @board.update(board_update_params)
+      redirect_to admin_board_path(@board), success: '掲示板を更新しました'
+    else
+      flash.now['danger'] = '掲示板を更新できませんでした'
+      render :edit
     end
   end
+
+  def destroy
+    @board.destroy
+    redirect_to admin_boards_path, success: '掲示板を削除しました'
+  end
+
+  private
+
+  def board_update_params
+    params.require(:board).permit(:title, :body, :user_id, :board_image, :board_image_cache)
+  end
+
+  def set_board
+    @board = Board.find(params[:id])
+  end
+end
--- config/initializers/ransack.rb
+++ config/initializers/ransack.rb
@@ -1,5 +1,5 @@
 Ransack.configure do |config|
-  config.add_predicate 'lteq_end_of_day',  #設定するpredicateに名前をつける
-                      arel_predicate: 'lteq',  #使いたいpredicate
-                      formatter: proc { |v| v.end_of_day } # 受け取った値をどうフォーマットするか
+  config.add_predicate 'lteq_end_of_day',
+                      arel_predicate: 'lteq',
+                      formatter: proc { |v| v.end_of_day }
 end
