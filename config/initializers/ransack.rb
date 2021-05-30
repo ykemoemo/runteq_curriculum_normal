f2167a96dc1ffc65a502dbc9f47433fd584c30a6
Yamashita <yk.emoemo@gmail.com>
2021-05-30 12:23:38 +0900

    [Add] admin user board crud

--- config/initializers/ransack.rb
+++ config/initializers/ransack.rb
@@ -1,5 +1,3 @@
 Ransack.configure do |config|
-  config.add_predicate 'lteq_end_of_day',
-                      arel_predicate: 'lteq',
-                      formatter: proc { |v| v.end_of_day }
+  config.add_predicate 'lteq_end_of_day', arel_predicate: 'lteq', formatter: proc { |v| v.end_of_day }
 end
